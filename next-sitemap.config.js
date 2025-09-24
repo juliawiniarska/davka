212857d5c81a86785c6630b01f943d2c42876936
Julia <julciagim@gmail.com>
2025-09-24 18:05:05 +0200

    commit

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,4 +5,11 @@ module.exports = {
   sitemapSize: 5000,
   changefreq: 'weekly',
   priority: 0.7,
+  additionalPaths: async (config) => [
+    await config.transform(config, '/'),
+    await config.transform(config, '/menu'),
+    await config.transform(config, '/daily'),
+    await config.transform(config, '/about'),
+    await config.transform(config, '/contact'),
+  ],
 }
